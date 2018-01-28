fe6563a084282919683a71fb6a78ee20b9382bfb
Laurent Barraud <contact@xtrmaddons.com>
2018-01-28 02:19:34 +0100

    Under construction

--- XtrmAddons.Net.Picture/Properties/AssemblyInfo.cs
+++ XtrmAddons.Net.Picture/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.18022.1713")]
-[assembly: AssemblyFileVersion("1.0.18022.1713")]
+[assembly: AssemblyVersion("1.0.18026.1623")]
+[assembly: AssemblyFileVersion("1.0.18026.1623")]
