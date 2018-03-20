b4f1d865de427bece6df52fd73226b84ba894edb
Laurent Barraud <shimsao@hotmail.com>
2018-03-20 17:58:13 +0100

    Under construction

---
+++ XtrmAddons.Net.Picture/Classes/Win32Icon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using XtrmAddons.Net.Picture.ExtractLargeIconFromFile.Shell;
+
+namespace XtrmAddons.Net.Picture.Classes
+{
+    public class Win32Icon
+    {
+        public const uint SHGFI_ICON = 0x100;
+        public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
+        public const uint SHGFI_SMALLICON = 0x1; // 'Small icon
+
+        [DllImport("shell32.dll")]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fullname"></param>
+        /// <param name="shgfi"></param>
+        /// <returns></returns>
+        public static Icon IconFromHandle(string fullname, uint shgfi = SHGFI_SMALLICON)
+        {
+            //Use this to get the small Icon
+            IntPtr hImgSmall;
+            SHFILEINFO shinfo = new SHFILEINFO();
+            hImgSmall = SHGetFileInfo(fullname, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFI_ICON | shgfi);
+
+            //The icon is returned in the hIcon member of the shinfo struct
+            return Icon.FromHandle(shinfo.hIcon);
+        }
+
+    }
+}
---
+++ XtrmAddons.Net.Windows/Converter/Picture/ConverterExtractLargeIconFromDirectory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using XtrmAddons.Net.Picture.Extensions;
+using XtrmAddons.Net.Picture.ExtractLargeIconFromFile;
+
+namespace XtrmAddons.Net.Windows.Converter.Picture
+{
+    /// <summary>
+    /// <para>Class Net UI Converter Picture Base.</para>
+    /// </summary>
+    public class ConverterExtractLargeIconFromDirectory : IValueConverter
+    {
+        /// <summary>
+        /// Method to convert string path of the Directory into bitmap image. 
+        /// </summary>
+        /// <param name="value">The binding object path of the picture.</param>
+        /// <param name="targetType">The target type for binding.</param>
+        /// <param name="parameter">Parameter for convert.</param>
+        /// <param name="culture">The culture to convert.</param>
+        /// <returns>A bitmap image for image binding.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var size = ShellEx.IconSizeEnum.ExtraLargeIcon;
+            return ShellEx.GetBitmapFromFolderPath((string)value, size).ToBitmapImage();
+        }
+
+        /// <summary>
+        /// Method to convert bitmap image into string Directory of the file. 
+        /// </summary>
+        /// <param name="value">The binding object path of the picture.</param>
+        /// <param name="targetType">The target type for binding.</param>
+        /// <param name="parameter">Parameter for convert.</param>
+        /// <param name="culture">The culture to convert.</param>
+        /// <returns>throw Not Implemented Exception.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
