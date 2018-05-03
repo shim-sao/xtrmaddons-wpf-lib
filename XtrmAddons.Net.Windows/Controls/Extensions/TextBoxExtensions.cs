1fa0d8812e2ed8fbc946a02c4366b076091ac087
Laurent Barraud <shimsao@hotmail.com>
2018-05-03 14:54:08 +0200

    Under construction

---
+++ XtrmAddons.Net.Windows/Controls/Extensions/TextBoxExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace XtrmAddons.Net.Windows.Controls.Extensions
+{
+    /// <summary>
+    /// Class XtrmAddons Windows Controls TextBox Extensions.
+    /// </summary>
+    public static class TextBoxExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Method to clear invalid TextBox base on DependencyProperty Binding.
+        /// </summary>
+        /// <param name="tb">The TextBox.</param>
+        /// <param name="dp">A TextBox DependencyProperty where a Binding is attached.</param>
+        public static void ValidationClear(this TextBox tb, DependencyProperty dp = null)
+        {
+            dp = dp ?? TextBox.TextProperty;
+            Validation.ClearInvalid(BindingOperations.GetBindingExpression(tb, dp));
+        }
+
+        #endregion
+    }
+}
---
+++ XtrmAddons.Net.Windows/ValidationRules/StringRequired.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+
+namespace XtrmAddons.Net.Windows.ValidationRules
+{
+    /// <summary>
+    /// <para>Class XtrmAddons Net Windows Validation Rule String Required.</para>
+    /// <para>Check if a string is not null, empty or whitespace.</para>
+    /// </summary>
+    public class StringRequired : ValidationRule
+    {
+        /// <summary>
+        /// Method to validate rule to apply to the object.
+        /// </summary>
+        /// <param name="value">A string.</param>
+        /// <param name="cultureInfo">The culture informations.</param>
+        /// <returns>True if conditions are validated otherwise false.</returns>
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string str = value as string;
+
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Value must not be null, empty or whitespace.");
+            }
+        }
+    }
+}
