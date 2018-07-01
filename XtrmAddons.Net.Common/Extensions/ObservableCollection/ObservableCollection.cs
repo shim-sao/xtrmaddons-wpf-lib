58964ed70c28a69ce7eeb5683823ed78c329312a
Laurent Barraud <shimsao@hotmail.com>
2018-07-01 15:12:20 +0200

    class contraint removed on Exists method.

--- XtrmAddons.Net.Common/Extensions/ObservableCollection/ObservableCollection.cs
+++ XtrmAddons.Net.Common/Extensions/ObservableCollection/ObservableCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -10,6 +11,18 @@ namespace XtrmAddons.Net.Common.Extensions
     /// </summary>
     public static class ObservableCollectionExtension
     {
+        #region Variables
+        
+        /// <summary>
+        /// Variable logger.
+        /// </summary>
+        private static readonly log4net.ILog log =
+        	log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        #endregion
+
+
+
         #region Methods
 
         /// <summary>
@@ -19,8 +32,15 @@ namespace XtrmAddons.Net.Common.Extensions
         /// <param name="collection">The collection to search in.</param>
         /// <param name="predicate">The predicate matching to check.</param>
         /// <returns>True if an item or more is found otherwise false.</returns>
-        public static bool Exists<T>(this ObservableCollection<T> collection, Predicate<T> predicate) where T : class
+        public static bool Exists<T>(this ObservableCollection<T> collection, Predicate<T> predicate)
         {
+            if (predicate == null)
+            {
+                ArgumentNullException e = new ArgumentNullException($"'{nameof(predicate)}' type of '{typeof(Predicate<T>)}' must not be null.");
+                log.Error(e.Output(), e);
+                throw e;
+            }
+
             return collection.ToList().Exists(predicate);
         }
 
@@ -31,8 +51,15 @@ namespace XtrmAddons.Net.Common.Extensions
         /// <param name="collection">The collection to search in.</param>
         /// <param name="match">The predicate matching to check.</param>
         /// <returns>True if an item or more is found otherwise false.</returns>
-        public static bool Exists<T>(this ObservableCollection<T> collection, Func<T, bool> match) where T : class
+        public static bool Exists<T>(this ObservableCollection<T> collection, Func<T, bool> match)
         {
+            if (match == null)
+            {
+                ArgumentNullException e = new ArgumentNullException($"'{nameof(match)}' type of '{typeof(Predicate<T>)}' must not be null.");
+                log.Error(e.Output(), e);
+                throw e;
+            }
+
             return collection.Exists(new Predicate<T>(match));
         }
 
